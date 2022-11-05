e6628e5213c78df0aa3cd05ffd059d3eb91452c7
santiago fernandez fernandez <santifernandez0902@gmail.com>
2022-11-04 20:11:01 -0500

    New: i finish point 2 and 3

--- view/EvaluacionActaPartial.py
+++ view/EvaluacionActaPartial.py
@@ -10,7 +10,7 @@ from controller.ControladorPDF import ControladorPdf
 def agregar_acta(st, controlador):
     st.title("Generación De Actas")
     col1, col2, col3 = st.columns(3)
-    col5, col6, col7, col8 = st.columns(4)
+    col5, col6, col7, col8 ,col9= st.columns(5)
     # Objeto que modelará el formulario
     info_acta_obj = InfoActa(controlador.criterios)
     info_acta_obj.fecha_acta = datetime.today().strftime('%Y-%m-%d')
@@ -28,6 +28,8 @@ def agregar_acta(st, controlador):
         info_acta_obj.jurado1 = st.text_input("Jurado #1")
     with col8:
         info_acta_obj.jurado2 = st.text_input("Jurado #2")
+    with col9:
+        info_acta_obj.fecha_presentacion= st.text_input("Ingrese la fecha de presentacion")
     enviado_btn = st.button("Enviar")
 
     # Cuando se oprime el botón se agrega a la lista
@@ -55,8 +57,8 @@ def ver_historico_acta(st, controlador):
         st.write("#### Acta #", numero)
         numero += 1
         col1, col2, col3, col4 = st.columns(4)
-        col5, col6, col7, col8 = st.columns(4)
-        col9, col10 = st.columns(2)
+        col5, col6, col7, col8, col9 = st.columns(5)
+        col10, col11= st.columns(2)
         with col1:
             st.write("**Autor**")
             st.write(acta.autor)
@@ -82,6 +84,9 @@ def ver_historico_acta(st, controlador):
             st.write("**Jurado #2**")
             st.write(acta.jurado2)
         with col9:
+            st.write("**Fecha De Presentacion**")
+            st.write(acta.fecha_presentacion)
+        with col10:
             st.write("**Nota Final**")
             if not acta.estado:
                 st.write("Sin nota")
@@ -89,7 +94,7 @@ def ver_historico_acta(st, controlador):
                 st.write(acta.nota_final, "Acta Aprobada")
             else:
                 st.write(acta.nota_final, "Acta Reprobada")
-        with col10:
+        with col11:
             st.write("**Estado**")
             if not acta.estado:
                 st.write("Acta pendiente por calificar")
